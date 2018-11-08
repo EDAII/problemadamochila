7910ecd4ab1e6fb297e5983be46f20bd51d5acf4
emanueng <emanuelhbga@gmail.com>
2018-11-08 18:48:05 -0200

    fechamento do problema

--- problemadamochila3 (1).py
+++ problemadamochila3 (1).py
@@ -50,14 +50,42 @@ def retornamaior( i , i2,A):
     if(A[i][1]>A[i2][1]):
       return A[i][1]
     else:
-      return A[i2][1]               
+      return A[i2][1]       
+      
+def capacidademaior(i2,i,matrizvaltot,A,pesomenorlista,valor):
+            if(i2==pesomenorlista[0][1]):
+              matrizvaltot[i][i2]=A[pesomenorlista[0][0]][1] 
+              i2=i2-pesomenorlista[0][1]  
+              valor=valor+A[pesomenorlista[0][0]][1]  
+            elif(i2==pesomenorlista[0][1]):
+              print(pesomenorlista[1][0])
+              if(i2==pesomenorlista[1][1]):
+                 valor=retornamaior(pesomenorlista[0][0],pesomenorlista[1][0],A)
+                 i2=i2-pesomenor[1][1]                  
+            elif(len(pesomenorlista)>=2):   
+                if(i2>pesomenorlista[0][1] and i2<pesomenorlista[1][1]):
+                   valor=A[pesomenorlista[0][0]][1]
+                else:
+                   if(A[i][2]==i2):
+                      if(A[i][1]>matrizvaltot[i-1][1]):
+                         valor=A[i][1]
+                         i2=i2-A[i][2]      
+                      else:
+                         valor=matrizvaltot[i-1][1]    
+                   else:
+                    if(A[i][2]<i2):
+                        i2=i2-A[i][2]
+                        valor=valor+A[i][2]
+                        matrizresultado=eliminaelemento(A,i)
+                        valor=capacidademaior(i2,i,matrizvaltot,matrizresultado,pesomenorlista,valor)
+            return valor                                 
 def main():                                                                             
     a=input("Digite a quantidade de elementos")
     A=[]
     colunas=int(a)
     c=input("Digite a capacidade da mochila")
     d=int(c)      
-    for i in range(colunas):
+    for i in range(colunas+1):
       valor=randint(1,100)
       peso=randint(1,d)  
       A.append([0]*3)
@@ -79,6 +107,8 @@ def main():
               pesomenorlista.append(encontramenorpeso(matrizresultado))
               matrizrestante=eliminaelemento(A,pesomenorlista[0][0])    
               pesomenorlista.append(encontramenorpeso(matrizrestante))
+              if(pesomenorlista[1][1]>pesomenorlista[0][0]):
+                 pesomenorlista[1][1]=pesomenorlista[1][1]+1          
               print(" este e o o segundo menor peso :",pesomenorlista[1][1])     
           if(pesomenorlista[0][1]<=i2 and i==1):
              matrizvaltot[i][i2]=matrizvaltot[i][i2]=A[pesomenorlista[0][0]][1]     
@@ -88,9 +118,19 @@ def main():
             elif(i2==pesomenorlista[0][1]):
               print(pesomenorlista[1][0])
               if(i2==pesomenorlista[1][1]):
-                 matrizvaltot[i][i2]=retornamior(pesomenorlista[0][0],pesomenorlista[1][0])                  
+                 matrizvaltot[i][i2]=retornamaior(pesomenorlista[0][0],pesomenorlista[1][0],A)                  
             elif(len(pesomenorlista)>=2):   
                 if(i2>pesomenorlista[0][1] and i2<pesomenorlista[1][1]):
-                    matrizvaltot[i][i2]=A[pesomenorlista[0][0]][1]
+                    matrizvaltot[i][i2]=matrizresultado[pesomenorlista[0][0]][1]
+                else:
+                   if(A[i][2]==i2):
+                      if(A[i][1]>matrizvaltot[i-1][1]):
+                         matrizvaltot[i][i2]=A[i][1]
+                      else:
+                         matrizvaltot[i][i2]=matrizvaltot[i-1][1]    
+                      print(matrizvaltot[i][i2])
+                   else:
+                      valor2=capacidademaior(i2,i,matrizvaltot,matrizresultado,pesomenorlista,0)              
+                     
     print(matrizvaltot)            
 main()
