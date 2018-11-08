0fcf355d69b6cbc4767c7b60de909db6646389c4
emanueng <emanuelhbga@gmail.com>
2018-11-08 14:21:39 -0200

    rearmundo o codigo

--- problemadamochila3 (1).py
+++ problemadamochila3 (1).py
@@ -40,12 +40,17 @@ def eliminaelemento(A,i):
     return b 
 def selecionatamanho( A,i2):
     b=[]
-    posicao=0  
+    posicao=0      
     for Aele in A:
       if(posicao<=i2):
         b.append(Aele)
       posicao=posicao+1
-    return b              
+    return b 
+def retornamaior( i , i2,A):
+    if(A[i][1]>A[i2][1]):
+      return A[i][1]
+    else:
+      return A[i2][1]               
 def main():                                                                             
     a=input("Digite a quantidade de elementos")
     A=[]
@@ -71,21 +76,21 @@ def main():
           if(i==0 or i2==0):
             matrizvaltot[i][i2]=0
           if(len(pesomenorlista)==0):
-               pesomenorlista.append(encontramenorpeso(matrizresultado))   
+              pesomenorlista.append(encontramenorpeso(matrizresultado))
+              matrizrestante=eliminaelemento(A,pesomenorlista[0][0])    
+              pesomenorlista.append(encontramenorpeso(matrizrestante))
+              print(" este e o o segundo menor peso :",pesomenorlista[1][1])     
           if(pesomenorlista[0][1]<=i2 and i==1):
              matrizvaltot[i][i2]=matrizvaltot[i][i2]=A[pesomenorlista[0][0]][1]     
-          else: 
+          else:      
             if(i2==pesomenorlista[0][1]):
               matrizvaltot[i][i2]=A[pesomenorlista[0][0]][1] 
-            elif(i2>pesomenorlista[0][1]):
-              matrizrestante=eliminaelemento(A,pesomenorlista[0][0]) 
-              pesomenorlista.append(encontramenorpeso(matrizrestante))
-              print(pesomenorlista[1][0]) 
+            elif(i2==pesomenorlista[0][1]):
+              print(pesomenorlista[1][0])
               if(i2==pesomenorlista[1][1]):
-                 if(A[pesomenorlista[0][0]][1]>A[pesomenorlista[1][0]][1]):
-                  matrizvaltot[i][i2]=A[pesomenorlista[0][0]][1]
-                 else:
-                  matrizvaltot[i][i2]=A[pesomenorlista[1][0]][1]
-                            
-    print(matrizvaltot)   
+                 matrizvaltot[i][i2]=retornamior(pesomenorlista[0][0],pesomenorlista[1][0])                  
+            elif(len(pesomenorlista)>=2):   
+                if(i2>pesomenorlista[0][1] and i2<pesomenorlista[1][1]):
+                    matrizvaltot[i][i2]=A[pesomenorlista[0][0]][1]
+    print(matrizvaltot)            
 main()
